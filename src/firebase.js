e88da99b7834fbb420d0eb42320159990e879db5
david-braga <bragav.david@gmail.com>
2022-02-23 16:25:16 +1300

    added navbar

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAgkDpqGHaE4ypuJ1ktONxFG2Ea45dZhs",
+    authDomain: "clone-e3fa6.firebaseapp.com",
+    projectId: "clone-e3fa6",
+    storageBucket: "clone-e3fa6.appspot.com",
+    messagingSenderId: "701784073979",
+    appId: "1:701784073979:web:3b4713b18155310ab6325e",
+    measurementId: "G-Q1X95TEYHV"
+};
